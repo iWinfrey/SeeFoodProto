fe6e76e913141f65aa0e65ad907218fbe44514cd
madison_yancey <yancey.12@wright.edu>
2018-12-02 16:57:09 -0500

    Toolbar and updated stats screen

--- app/src/main/java/com/example/bioni/seefoodprotov2/StatsActivity.kt
+++ app/src/main/java/com/example/bioni/seefoodprotov2/StatsActivity.kt
@@ -2,11 +2,37 @@ package com.example.bioni.seefoodprotov2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import com.example.bioni.seefoodprotov2.R.id.toolbar
+import android.R.attr.button
+import android.view.Gravity
+import android.R.attr.gravity
+import android.app.ActionBar
+import android.content.Intent
+import android.view.ViewGroup.LayoutParams.FILL_PARENT
+import android.widget.Button
+import android.widget.LinearLayout
+
+
 
 class StatsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val bt = Button(this)
+        bt.setText("Refresh")
+        toolbar.addView(bt)
+
+        /*openHistoryButton.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java).apply {
+                putExtra("Thing to do", "Start Historying!") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }*/
     }
 }
