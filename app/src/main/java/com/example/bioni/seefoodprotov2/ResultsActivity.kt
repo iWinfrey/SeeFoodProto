2c24286257de8e5404e914699d717ba96f9ab175
madison_yancey <yancey.12@wright.edu>
2018-12-03 00:43:35 -0500

    Graphical representation of classification and UI fixes

--- app/src/main/java/com/example/bioni/seefoodprotov2/ResultsActivity.kt
+++ app/src/main/java/com/example/bioni/seefoodprotov2/ResultsActivity.kt
@@ -1,18 +1,72 @@
 package com.example.bioni.seefoodprotov2
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar
+import com.example.bioni.seefoodprotov2.R.id.textView
 
 class ResultsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val btnHome = Button(this)
+        btnHome.setText("Home")
+        btnHome.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnHome.setTextColor(Color.WHITE)
+        toolbar.addView(btnHome)
+
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("Thing to do", "Go Home") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }
+
+        val btnHistory = Button(this)
+        btnHistory.setText("History")
+        btnHistory.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnHistory.setTextColor(Color.WHITE)
+        toolbar.addView(btnHistory)
+
+        btnHistory.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java).apply {
+                putExtra("Thing to do", "History") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }
+
+        val btnStats = Button(this)
+        btnStats.setText("Stats")
+        btnStats.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnStats.setTextColor(Color.WHITE)
+        toolbar.addView(btnStats)
+
+        btnStats.setOnClickListener {
+            val intent = Intent(this, StatsActivity::class.java).apply {
+                putExtra("Thing to do", "Stats") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }
+
+        /*val openMainActivity = findViewById<Button>(R.id.go_home_btn)
+        openMainActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("Thing to do", "Going back to main activity!") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }*/
 
         var previewBox = findViewById<ImageView>(R.id.image_preview2)
         previewBox.setImageURI(Uri.fromFile(CandidateImage.file))
@@ -21,18 +75,34 @@ class ResultsActivity : AppCompatActivity() {
         val resultText = "Is it food?... " + CandidateImage.classification
         classificationText.text = resultText
 
-        val openMainActivity = findViewById<Button>(R.id.go_home_btn)
-        openMainActivity.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java).apply {
-                putExtra("Thing to do", "Going back to main activity!") // what does this do? nobody knows
-            }
-            startActivity(intent)
+        val progress1 = findViewById<View>(R.id.progress_1) as RoundCornerProgressBar
+        progress1.setProgressBackgroundColor(Color.parseColor("#ffffff"))
+        progress1.setMax(60F)
+
+        if(resultText.equals("Is it food?... High No")){
+            progress1.setProgressColor(Color.parseColor("#b20000"))
+            progress1.setProgress(10F)
+        } else if(resultText.equals("Is it food?... Moderate No")){
+            progress1.setProgressColor(Color.parseColor("#ff4500"))
+            progress1.setProgress(20F)
+        } else if(resultText.equals("Is it food?... Low No")){
+            progress1.setProgressColor(Color.parseColor("#ffff33"))
+            progress1.setProgress(30F)
+        } else if(resultText.equals("Is it food?... Low Yes")){
+            progress1.setProgressColor(Color.parseColor("#228B22"))
+            progress1.setProgress(40F)
+        } else if(resultText.equals("Is it food?... Moderate Yes")){
+            progress1.setProgressColor(Color.parseColor("#3232cd"))
+            progress1.setProgress(50F)
+        } else if(resultText.equals("Is it food?... High Yes")){
+            progress1.setProgressColor(Color.parseColor("##4b0082"))
+            progress1.setProgress(60F)
         }
 
         if (CandidateImage.source == "gallery") {
             // set actions for the navigation buttons
             val goBackButton = findViewById<Button>(R.id.go_back_btn2)
-            goBackButton.text = "Select another pic"
+            goBackButton.text = "Select another picture"
             goBackButton.setOnClickListener {
                 val intent = Intent(this, MainActivity::class.java).apply {
                     putExtra("do_thing", "gallery") // what does this do? nobody knows
@@ -42,7 +112,7 @@ class ResultsActivity : AppCompatActivity() {
         } else {
             // set actions for the navigation buttons
             val goBackButton = findViewById<Button>(R.id.go_back_btn2)
-            goBackButton.text = "Take another pic"
+            goBackButton.text = "Take another picture"
             goBackButton.setOnClickListener {
                 val intent = Intent(this, MainActivity::class.java).apply {
                     putExtra("do_thing", "camera") // what does this do? nobody knows
--- app/src/main/java/com/example/bioni/seefoodprotov2/StatsActivity.kt
+++ app/src/main/java/com/example/bioni/seefoodprotov2/StatsActivity.kt
@@ -1,18 +1,18 @@
 package com.example.bioni.seefoodprotov2
 
+import android.R.attr.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import com.example.bioni.seefoodprotov2.R.id.toolbar
-import android.R.attr.button
 import android.view.Gravity
-import android.R.attr.gravity
 import android.app.ActionBar
 import android.content.Intent
+import android.graphics.Color
 import android.view.ViewGroup.LayoutParams.FILL_PARENT
 import android.widget.Button
 import android.widget.LinearLayout
-
+import okhttp3.Request
 
 
 class StatsActivity : AppCompatActivity() {
@@ -24,15 +24,67 @@ class StatsActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val bt = Button(this)
-        bt.setText("Refresh")
-        toolbar.addView(bt)
+        val btnHome = Button(this)
+        btnHome.setText("Home")
+        btnHome.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnHome.setTextColor(Color.WHITE)
+        toolbar.addView(btnHome)
 
-        /*openHistoryButton.setOnClickListener {
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("Thing to do", "Go Home") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }
+
+        val btnHistory = Button(this)
+        btnHistory.setText("History")
+        btnHistory.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnHistory.setTextColor(Color.WHITE)
+        toolbar.addView(btnHistory)
+
+        btnHistory.setOnClickListener {
             val intent = Intent(this, HistoryActivity::class.java).apply {
-                putExtra("Thing to do", "Start Historying!") // what does this do? nobody knows
+                putExtra("Thing to do", "History") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }
+
+        //Having a stats button just seems redundant if we have a refresh one
+        /*val btnStats = Button(this)
+        btnStats.setText("Stats")
+        btnStats.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnStats.setTextColor(Color.WHITE)
+        toolbar.addView(btnStats)
+
+        btnStats.setOnClickListener {
+            val intent = Intent(this, StatsActivity::class.java).apply {
+                putExtra("Thing to do", "Stats") // what does this do? nobody knows
             }
             startActivity(intent)
         }*/
+
+        val btnRefresh = Button(this)
+        btnRefresh.setText("Refresh")
+        btnRefresh.setBackgroundColor(Color.parseColor("#4b0082"))
+        btnRefresh.setTextColor(Color.WHITE)
+        toolbar.addView(btnRefresh)
+
+        btnRefresh.setOnClickListener {
+            val intent = Intent(this, StatsActivity::class.java).apply {
+                putExtra("Thing to do", "Refresh") // what does this do? nobody knows
+            }
+            startActivity(intent)
+        }
+    }
+
+    private fun doStuff() {
+        val request = Request.Builder()
+            .url("http://3.16.73.99/upload")
+//            .url("http://192.168.0.200:5000/upload") // for local testing (insert your computer's local IP here)
+            .addHeader("Content-Type", "application/json")
+            .build()
+
+
     }
 }
